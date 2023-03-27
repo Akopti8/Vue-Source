2ed09d2af4b26c191157e72eb1986af491c12339
“Anton <bccomputing@eagles.bridgewater.edu>
2023-03-27 19:50:37 -0400

    app13

---
+++ app3/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <MeaningofLife></MeaningofLife>
+ 
+  </div>
+  
+</template>
+<script>
+import MeaningofLife from './components/MeaningofLife.vue';
+
+
+export default {
+  components: {
+    MeaningofLife
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ app3/src/components/MeaningofLife.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="logo">
+    <img src="../assets/download.jpg" class="center">  
+  </div>
+</template>
+
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+</style>
